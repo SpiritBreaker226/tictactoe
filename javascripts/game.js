2861205367e867a1d8eb5c0358309da7106e26f8
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-23 16:59:50 -0500

    Add Current Turn To Game Class
    Add current turn to Game Class in order to know whose turn it is since
    this will be used to display the winner and the know who won.

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,14 +1,17 @@
 var Game = function() {
 	this.turn = 1;
+	this.currentTurn = "X";
 }
 
 Game.prototype.gameLoop = function(currentSelectedELement) {
 	if((this.turn % 2) == 1) {
-		currentSelectedELement.text("x");
+		this.currentTurn = "X";
 	} else {
-		currentSelectedELement.text("o");
+		this.currentTurn = "O";
 	};
-	
+
+	currentSelectedELement.text(this.currentTurn);
+
 	this.turn++;
 	currentSelectedELement.off("click");
 }
